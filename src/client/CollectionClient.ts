77777543556b819a98cedcc574c70ab6e4a5947b
Prince Patoliya <princepatoliya852@gmail.com>
2022-05-25 12:17:24 +0530

    - add all collection and format file

--- src/client/CollectionClient.ts
+++ src/client/CollectionClient.ts
@@ -28,4 +28,18 @@ export class CollectionClient extends BaseClient {
       throw new Error(`Failed to get collection with id ${id}. Error: ${JSON.stringify(error)}`);
     }
   }
+
+  async getAll(): Promise<Collection> {
+    const url = `${APIConstants.API_BASE_URL}/collections`;
+
+    try {
+      const { data } = await this._get<Collection>(url, null, {
+        [APIConstants.ZECKO_ACCESS_TOKEN_HEADER_KEY]: this.accessToken,
+      });
+
+      return data;
+    } catch (error) {
+      throw new Error(`Failed to get all collection Error: ${JSON.stringify(error)}`);
+    }
+  }
 }
