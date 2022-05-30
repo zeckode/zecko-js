66917ab5db2ff05ed7fa8bd891175be0984263ca
akartik80 <akartik80@gmail.com>
2022-05-30 12:28:56 +0530

    minor changes

--- src/client/Zecko.ts
+++ src/client/Zecko.ts
@@ -18,41 +18,39 @@ export class Zecko {
    * @param sdkParams Pass your accessToken here.
    * <br>
    * <h2>Usage examples:</h2>
-   *
-   * <h3>Collection</h3>
-   * <b>Get collection by ID</b>
-   * <br><br>
-   *
-   * ```
-   * const zecko = new Zecko({ accessToken: 'YOUR_ACCESS_TOKEN' });
-   * return zecko.collectionClient.get(id);
-   * ```
-   *
+   * <h3>Collections</h3>
    * <b>Get all collections</b>
+   * <br><br>
    *
    * ```
    * const zecko = new Zecko({ accessToken: 'YOUR_ACCESS_TOKEN' });
    * return zecko.collectionClient.getAll();
    * ```
    *
-   * <h3>Product</h3>
-   * <b>Get product by ID</b>
-   * <br><br>
+   * <b>Get collection by ID</b>
    *
    * ```
    * const zecko = new Zecko({ accessToken: 'YOUR_ACCESS_TOKEN' });
-   * return zecko.productClient.get(id);
+   * return zecko.collectionClient.get('YOUR_COLLECTION_ID');
    * ```
-   *
+   * <h3>Products</h3>
    * <b>Get all products</b>
+   * <br><br>
    *
    * ```
    * const zecko = new Zecko({ accessToken: 'YOUR_ACCESS_TOKEN' });
    * return zecko.productClient.getAll();
    * ```
    *
+   * <b>Get product by ID</b>
+   *
+   * ```
+   * const zecko = new Zecko({ accessToken: 'YOUR_ACCESS_TOKEN' });
+   * return zecko.productClient.get('YOUR_PRODUCT_ID');
+   * ```
+   *
    * - All examples available at https://github.com/zeckode/zecko-js-examples/tree/main/src/examples/js <br>
-   * - For detailed documentation, visit respective class docs.
+   * - For detailed documentation, visit respective class docs below.
    */
   public constructor(sdkParams: SDKParams) {
     if (!sdkParams || !sdkParams.accessToken) {
