20bafc50bda9fc031a09bf6bf944b019e5844b81
princepatoliya <princepatoliya852@gmail.com>
2022-06-12 21:32:41 +0530

    - remove url

--- src/constants/APIConstants.ts
+++ src/constants/APIConstants.ts
@@ -2,7 +2,7 @@
  * @ignore
  */
 export class APIConstants {
-  static readonly API_BASE_URL: string = process.env.ZECKO_API_BASE_URL || 'http://localhost:5000/retailer/v1';
+  static readonly API_BASE_URL: string = process.env.ZECKO_API_BASE_URL || 'https://api.zecko.co/retailer/v1';
 
   static readonly ZECKO_ACCESS_TOKEN_HEADER_KEY = 'X-ZECKO-ACCESS-TOKEN';
 }
