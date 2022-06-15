567e767c6522250bad755583ec726b6428f74b0d
princepatoliya <princepatoliya852@gmail.com>
2022-06-15 18:12:31 +0530

    'before' changes done for product client

--- src/client/ProductClient.ts
+++ src/client/ProductClient.ts
@@ -14,6 +14,11 @@ export class ProductClient extends BaseClient {
   /**
    * @param collectionId Collection Id
    *
+   * @param before
+   * If `data.products.pageInfo.hasPreviousPage` is `true`, then request previous page by passing
+   * `data.products.pageInfo.startCursor` as `before` parameter. You can also pass `data.products.edges[i].cursor`
+   * as `before` parameter to get results before that cursor.
+   *
    * @param after
    * If `data.products.pageInfo.hasNextPage` is `true`, then request next page by passing
    * `data.products.pageInfo.endCursor` as `after` parameter. You can also pass `data.products.edges[i].cursor`
@@ -22,9 +27,10 @@ export class ProductClient extends BaseClient {
    * @returns {Promise<ProductsData>}
    * Paginated products list of maximum 20 products. To request further products, use `after` parameter.
    */
-  async getAllByCollectionId(collectionId: string, after?: string): Promise<ProductsData> {
+  async getAllByCollectionId(collectionId: string, before?: string, after?: string): Promise<ProductsData> {
     const params = new Object({
       collectionId: collectionId,
+      before: before,
       after: after,
     });
     const url = `${APIConstants.API_BASE_URL}/products`;
@@ -37,10 +43,20 @@ export class ProductClient extends BaseClient {
   /**
    * @param id Product Id
    *
+   * @param imagesBefore
+   * If `data.product.images.pageInfo.hasPreviousPage` is `true`, then request previous page by passing
+   * `data.product.images.pageInfo.startCursor` as `imagesBefore` parameter. You can also pass `data.product.images.edges[i].cursor`
+   * as `imagesBefore` parameter to get results before that cursor.
+   *
    * @param imagesAfter
    * If `data.product.images.pageInfo.hasNextPage` is `true`, then request next page by passing
    * `data.product.images.pageInfo.endCursor` as `imagesAfter` parameter. You can also pass `data.product.images.edges[i].cursor`
    * as `imagesAfter` parameter to get results after that cursor.
+   *
+   * @param variantsBefore
+   * If `data.product.variants.pageInfo.hasPreviousPage` is `true`, then request previous page by passing
+   * `data.product.variants.pageInfo.startCursor` as `variantsBefore` parameter. You can also pass `data.product.variants.edges[i].cursor`
+   * as `variantsBefore` parameter to get results before that cursor.
    *
    * @param variantsAfter
    * If `data.product.variants.pageInfo.hasNextPage` is `true`, then request next page by passing
@@ -51,9 +67,17 @@ export class ProductClient extends BaseClient {
    * Has a maximum of 10 images and 5 variants. To request further images and variants,
    * use `imagesAfter` and `variantsAfter` parameters respectively.
    */
-  async getById(id: string, imagesAfter?: string, variantsAfter?: string): Promise<ProductData> {
+  async getById(
+    id: string,
+    imagesBefore?: string,
+    imagesAfter?: string,
+    variantsBefore?: string,
+    variantsAfter?: string
+  ): Promise<ProductData> {
     const params = new Object({
+      imagesBefore: imagesBefore,
       imagesAfter: imagesAfter,
+      variantsBefore: variantsBefore,
       variantsAfter: variantsAfter,
     });
     const url = `${APIConstants.API_BASE_URL}/products/${id}`;
