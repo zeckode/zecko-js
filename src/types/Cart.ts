c9bb1754c9c84b78551ce5eda4ad5cd8ad9e2ee4
princepatoliya <princepatoliya852@gmail.com>
2022-08-25 14:36:31 +0530

    - add available Shipping rate type

--- src/types/Cart.ts
+++ src/types/Cart.ts
@@ -4,7 +4,7 @@ import { Discount } from './Discount';
 import { CurrencyCode, DraftOrderStatus, PaymentMethod } from './Enums';
 import { LineItemEdges } from './LineItem';
 import { Order } from './Order';
-import { ShippingLine } from './ShippingLine';
+import { ShippingLine } from './Shipping';
 import { TaxLine } from './TaxLine';
 import { UserError } from './UserError';
 
--- src/types/Shipping.ts
+++ src/types/Shipping.ts
@@ -1,3 +1,4 @@
+import { Money, MoneyBag } from './Money';
 import { TaxLine } from './TaxLine';
 
 export type ShippingLine = {
@@ -5,9 +6,16 @@ export type ShippingLine = {
   readonly code: string;
   readonly deliveryCategory: string;
   readonly id: string;
+  readonly originalPriceSet: MoneyBag;
   readonly phone: string;
   readonly shippingRateHandle: string;
   readonly source: string;
   readonly taxLines: readonly TaxLine[];
+  readonly title: string;
+};
+
+export type AvailableShippingRate = {
+  readonly handle: string;
+  readonly price: Money;
   readonly title: string;
 };
